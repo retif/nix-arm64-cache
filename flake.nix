575f16cdb3f00bc80630c14275698d4315bc1a1d
Oleks <oleks.kuksenko@gmail.com>
2026-03-10 23:00:26 +0200

    mirror nixos-ci flake: RPATH overlay + all deps (nix, attic, xonsh)

--- flake.nix
+++ flake.nix
@@ -1,20 +1,49 @@
 {
-  description = "Build nix 2.28.5 and attic-client for aarch64-linux";
+  description = "Build nixos-ci dependencies for aarch64-linux (mirrors nixos-ci flake)";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/2fc6539b481e1d2569f25f8799236694180c0993";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       system = "aarch64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (final: prev: {
+            # Mirror nixos-ci RPATH overlay: fix nix 2.28.5 broken RPATH
+            nixVersions = prev.nixVersions // {
+              nix_2_28 = prev.nixVersions.nix_2_28.overrideAttrs (old: {
+                nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ final.patchelf ];
+                postFixup = (old.postFixup or "") + ''
+                  for rpath in ${final.boost}/lib ${final.zstd.out}/lib ${final.libarchive.out}/lib; do
+                    patchelf --add-rpath "$rpath" $out/bin/nix
+                    for lib in $out/lib/lib*.so; do
+                      [ -f "$lib" ] && patchelf --add-rpath "$rpath" "$lib"
+                    done
+                  done
+                '';
+              });
+            };
+          })
+        ];
+      };
     in
     {
       packages.${system} = {
-        nix_2_28 = pkgs.nixVersions.nix_2_28;
-        attic-client = pkgs.attic-client;
-        default = pkgs.attic-client;
+        inherit (pkgs.nixVersions) nix_2_28;
+        inherit (pkgs) attic-client xonsh nix;
+        default = pkgs.symlinkJoin {
+          name = "nixos-ci-deps";
+          paths = [
+            pkgs.nixVersions.nix_2_28
+            pkgs.attic-client
+            pkgs.xonsh
+            pkgs.nix
+          ];
+        };
       };
     };
 }
